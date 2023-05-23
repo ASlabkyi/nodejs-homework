1152292939050154e5f0b6c0862071d7914c9eec
Andrii Slabkyi <aslabkyi@gmail.com>
2023-05-23 15:10:48 +0300

    1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const DB_HOST =
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(3001);
   })
   .then(() => console.log("Database connection successful"))
   .catch((error) => {
