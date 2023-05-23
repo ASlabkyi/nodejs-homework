4d2fcda206bd096229d81f7f680c46007b56d942
Andrii Slabkyi <aslabkyi@gmail.com>
2023-05-23 22:36:09 +0300

    add PORT

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-// const DB_HOST =
-//   "mongodb+srv://Andrii:o7rBFVEDfwILwd24@cluster0.rszr7o5.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .then(() => console.log("Database connection successful"))
   .catch((error) => {
