8454d4857835d77d67e3ca186cf557829eeb2acb
Andrii Slabkyi <aslabkyi@gmail.com>
2023-05-27 19:11:56 +0300

    final

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,6 +45,8 @@ const login = async (req, res) => {
 
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
 
+  await User.findByIdAndUpdate(user._id, { token });
+
   res.status(200).json({
     token,
     user: {
@@ -54,7 +56,39 @@ const login = async (req, res) => {
   });
 };
 
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+
+  res.status(200).json({
+    email,
+    subscription,
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.status(204).json({});
+};
+
+const updateSubscription = async (req, res) => {
+  const { _id } = req.user;
+
+  const result = await User.findByIdAndUpdate(_id, req.body, {
+    new: true,
+  });
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  updateSubscription: ctrlWrapper(updateSubscription),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth");
 
-const { validateBody } = require("../../middlewares");
+const {
+  validateBody,
+  authenticate,
+  validateSubscription,
+} = require("../../middlewares");
 
 const { schemas } = require("../../models/user");
 
@@ -11,5 +15,16 @@ const router = express.Router();
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+
+router.get("/current", authenticate, ctrl.getCurrent);
+
+router.post("/logout", authenticate, ctrl.logout);
+
+router.patch(
+  "/",
+  authenticate,
+  validateSubscription(schemas.updateSubscriptionSchema),
+  ctrl.updateSubscription
+);
 
 module.exports = router;
